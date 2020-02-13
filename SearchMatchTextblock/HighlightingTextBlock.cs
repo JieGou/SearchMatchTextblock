b93f30346dd55cd996e423190f4e96f7072e4f74
陈杰 <jiegou04@gmail.com>
2020-02-13 22:13:39 +0800

    改造成针对多个关键词也能正确匹配

--- SearchMatchTextblock/HighlightingTextBlock.cs
+++ SearchMatchTextblock/HighlightingTextBlock.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -55,65 +57,72 @@ namespace SearchMatchTextblock
 
         public int MatchCount
         {
-            get => (int) GetValue(MatchCountProperty);
+            get => (int)GetValue(MatchCountProperty);
             protected set => SetValue(MatchCountPropertyKey, value);
         }
 
         public Brush HighlightBackground
         {
-            get => (Brush) GetValue(HighlightBackgroundProperty);
+            get => (Brush)GetValue(HighlightBackgroundProperty);
             set => SetValue(HighlightBackgroundProperty, value);
         }
 
         public Brush HighlightForeground
         {
-            get => (Brush) GetValue(HighlightForegroundProperty);
+            get => (Brush)GetValue(HighlightForegroundProperty);
             set => SetValue(HighlightForegroundProperty, value);
         }
 
         public string HighlightText
         {
-            get => (string) GetValue(HighlightTextProperty);
+            get => (string)GetValue(HighlightTextProperty);
             set => SetValue(HighlightTextProperty, value);
         }
 
         public string Text
         {
-            get => (string) GetValue(TextProperty);
+            get => (string)GetValue(TextProperty);
             set => SetValue(TextProperty, value);
         }
 
         public TextWrapping TextWrapping
         {
-            get => (TextWrapping) GetValue(TextWrappingProperty);
+            get => (TextWrapping)GetValue(TextWrappingProperty);
             set => SetValue(TextWrappingProperty, value);
         }
 
         public TextTrimming TextTrimming
         {
-            get => (TextTrimming) GetValue(TextTrimmingProperty);
+            get => (TextTrimming)GetValue(TextTrimmingProperty);
             set => SetValue(TextTrimmingProperty, value);
         }
 
         private static void OnHighlightTextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            var textblock = (HighlightingTextBlock) d;
+            var textblock = (HighlightingTextBlock)d;
             textblock.ProcessTextChanged(textblock.Text, e.NewValue as string);
         }
 
         private static void OnTextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            var textblock = (HighlightingTextBlock) d;
+            var textblock = (HighlightingTextBlock)d;
             textblock.ProcessTextChanged(e.NewValue as string, textblock.HighlightText);
         }
 
         private void ProcessTextChanged(string mainText, string highlightText)
         {
             if (highlightTextBlock == null)
+            {
                 return;
+            }
+
             highlightTextBlock.Inlines.Clear();
             SetValue(MatchCountPropertyKey, 0);
-            if (highlightTextBlock == null || string.IsNullOrWhiteSpace(mainText)) return;
+            if (highlightTextBlock == null || string.IsNullOrWhiteSpace(mainText))
+            {
+                return;
+            }
+
             if (string.IsNullOrWhiteSpace(highlightText))
             {
                 var completeRun = new Run(mainText);
@@ -121,6 +130,25 @@ namespace SearchMatchTextblock
                 return;
             }
 
+            if (highlightText.Contains(" ") || highlightText.Contains(","))
+            {
+                var textArray = highlightText.Split(new string[] { " ", "," }
+                    , StringSplitOptions.RemoveEmptyEntries);
+                ColorBrushTextParts(mainText, textArray.ToList());
+            }
+            else
+            {
+                ColorBrushTextParts(mainText, new List<string>() { highlightText });
+            }
+        }
+
+        /// <summary>
+        /// 对匹配的文字着色
+        /// </summary>
+        /// <param name="mainText">文字</param>
+        /// <param name="highlightText">匹配的需要亮显的文字</param>
+        private void ColorBrushTextPart(string mainText, string highlightText)
+        {
             var find = 0;
             var searchTextLength = highlightText.Length;
             while (true)
@@ -148,6 +176,67 @@ namespace SearchMatchTextblock
             }
         }
 
+        /// <summary>
+        /// 对匹配的文字列表着色 即多个条件
+        /// </summary>
+        /// <param name="mainText">文字</param>
+        /// <param name="highlightTextList">匹配的需要亮显的文字</param>
+        private void ColorBrushTextParts(string mainText, List<string> highlightTextList)
+        {
+            if (highlightTextList.Count == 1)
+            {
+                ColorBrushTextPart(mainText, highlightTextList[0]);
+                return;
+            }
+
+            var find = 0;
+            while (true)
+            {
+                //能匹配的 需要亮显的字符串
+                var canBeMatchTextList =
+                    (from item in highlightTextList
+                     let index = mainText.IndexOf(item, find, StringComparison.InvariantCultureIgnoreCase)
+                     where index >= 0
+                     orderby index
+                     select item)
+                    .ToList();
+                if (canBeMatchTextList.Count == 0)
+                {
+                    break;
+                }
+
+                string highlightText = canBeMatchTextList.First();
+                var searchTextLength = highlightText.Length;
+
+                var oldFind = find;
+                find = mainText.IndexOf(highlightText, find, StringComparison.InvariantCultureIgnoreCase);
+                if (find == -1)
+                {
+                    highlightTextBlock.Inlines.Add(
+                        oldFind > 0
+                            ? GetRunForText(mainText.Substring(oldFind, mainText.Length - oldFind), false)
+                            : GetRunForText(mainText, false));
+                    break;
+                }
+
+                if (oldFind == find)
+                {
+                    highlightTextBlock.Inlines.Add(GetRunForText(mainText.Substring(oldFind, searchTextLength), true));
+                    SetValue(MatchCountPropertyKey, MatchCount + 1);
+                    find = find + searchTextLength;
+                    continue;
+                }
+
+                highlightTextBlock.Inlines.Add(GetRunForText(mainText.Substring(oldFind, find - oldFind), false));
+            }
+        }
+
+        /// <summary>
+        /// 为匹配的文字部分添加着色
+        /// </summary>
+        /// <param name="text">匹配的文字部分</param>
+        /// <param name="isHighlighted">是否亮显（着色）</param>
+        /// <returns></returns>
         private Run GetRunForText(string text, bool isHighlighted)
         {
             var textRun = new Run(text)
@@ -162,7 +251,10 @@ namespace SearchMatchTextblock
         {
             highlightTextBlock = GetTemplateChild(HighlighttextblockName) as TextBlock;
             if (highlightTextBlock == null)
+            {
                 return;
+            }
+
             ProcessTextChanged(Text, HighlightText);
         }
     }
